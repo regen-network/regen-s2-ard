1d05e06b00c6d309d9d9da976c1869e9ff88ddc6
matyasrada <rada.matyas@gmail.com>
2019-10-17 09:20:52 +0000

    Update s2-ard.sh with named argument parsing

--- scripts/s2-ard.sh
+++ scripts/s2-ard.sh
@@ -4,39 +4,74 @@
 
 docker run --name s2-ard -dit s2-ard
 
-tile=$1
+# Parse named argument options
+while :; do
+    case $1 in
+        -t|--tile)
+                if [ "$2" ]; then
+                        TILE=$2
+                        shift
+                else
+                        die 'ERROR: "--tile" requires a non-empty option argument.'
+                fi
+                ;;
+        -c|--config)
+                if [ "$2" ]; then
+                        CONFIG=$2
+                        shift
+                else
+                        die 'ERROR: "--config" requires a non-empty option argument.'
+                fi
+                ;;
+        -a|--aoi)
+                if [ "$2" ]; then
+                        AOI=$2
+                        shift
+                else
+                        die 'ERROR: "--aoi" requires a non-empty option argument.'
+                fi
+                ;;
+        *)
+                break
+    esac
 
-if [[ -d $1 ]]; 
+    shift
+done
+
+echo "Tile : $TILE"
+echo "Config : $CONFIG"
+echo "AOI : $AOI"
+
+if [[ -d $TILE ]];
 then
       echo "Copying SAFE Directory"
-      docker cp $tile s2-ard:work
-      tile="/work/"`basename "$tile"`
-      echo $tile
+      docker cp $TILE s2-ard:work
+      tile="/work/"`basename "$TILE"`
 else
       echo "not SAFE Directory"
 fi
 
-if [ -z "$2" ]
+if [ -z "$CONFIG" ]
 then
       echo "no config.yml file copied"
 else
-      echo "config.yml file copied"
-      docker cp $2 s2-ard:app
+      echo "Copying config.yml file"
+      docker cp $CONFIG s2-ard:app
 fi
 
 if [ -z "$3" ]
 then
-      echo "no aoi.geojson file copied"
+      echo "no aoi.geojson file given"
 else
-      echo "aoi.geojson file copied"
-      docker cp $3 s2-ard:app
+      echo "Copying aoi.geojson file"
+      docker cp $AOI s2-ard:app
 fi
 
 # execute pre-processing of the data product (tile)
-docker exec -it s2-ard bash -c "python /app/ard.py --tile "$tile""
+#docker exec -it s2-ard bash -c "python /app/ard.py --tile "$TILE""
 
 # copy output files/folders to host from s2-ard container
-docker cp s2-ard:output $PWD
+#docker cp s2-ard:output $PWD
 
 # remove files/folder from work and output directory on container
-docker exec s2-ard sh -c 'rm -rf /output/* /work/*'
+#docker exec s2-ard sh -c 'rm -rf /output/* /work/*'
